6c7e4c00501b480a52097cd9a051f51675204f98
shadi-git <44185622+shadi-git@users.noreply.github.com>
2021-04-06 15:59:13 +0300

    New class to accommodate savings/investing account
    New class to accommodate savings/investing account and features. See comments in code highlighting the approach to using a class. Better approaches may exist but this can serve as a starting point. More to be added to this class and to NewBank.java to incorporate it into src
    
    This cater to FR10 and Fr19 among others that target savings/investing features
    
    The basic functionality is to eventually have the option to deposit money into the investing account and earn interest on that money based on the type of investment account selected (three options are suggested in the comments) 
    
    Next up:
    - add util.calendar or similar to grow start amount by the rate of return each year
    - create doubles for each investment account type and its related rate of return
    - let client choose account type and start earning interest

---
+++ src/server/investingAccount.java
@@ -0,0 +1,58 @@
+//Re Fr19 and FR10 - clients can access savings/investing accounts that they can use to generate interest
+//this is an approach where an entire new class is created to accomodate the special features regarding investment/savings returns and such
+//Feel free to suggest better approaches to this feature
+//Kept 'mySavingsGoal' from account.java as a reminder to incorporate savings goal eventually into the investing account
+
+package server;
+
+public class Investing {
+	
+	private String investmentType;
+  //initial options: Guaranteed Savings Account Returns, Low-risk investment portfolio, Medium-risk investment portfolio
+	
+  private double startAmount;
+  
+	// come back to savings goal to incorporate it later : private double mySavingsGoal;
+  
+  private double returnRate;
+  //investment rate of return - assuming it's fixed and guaranteed for now
+  //initial options - per year rate of return: 
+  //Guaranteed Savings Account Returns = 1%
+  //Low-risk investment portfolio: %2
+  //Medium-risk investment portfolio: %2.75
+
+	public Account(String investmentType, double startAmount, double returnRate) {
+		this.investmentType = investmentType;
+		this.startAmount = startAmount;
+	}
+
+
+	public String toString() {
+		return (investmentType + ": " + startAmount);
+	}
+
+
+	public void investMoney(double amount ){
+		this.startAmount += amount;
+	}
+  
+  public String viewInvestmentTypes() {
+    
+    return ("Thanks for choosing to invest and grow your Savings with NewBank. Here are the investment options we currently offer:" + "1. Guaranteed Savings Account Returns = 1% \n"
+  + "2. Low-risk investment portfolio: %2 \n" + "3. Medium-risk investment portfolio: %2.75 \n"
+  }
+  
+  //option for client to view potnetial reutrns from an investment choice over a provided a number of years (add investment type as an arugment later - currently this only answers for investment type 1)
+  public void viewPotentialReturns(double years){
+    this.startAmount * ((1+0.01) * years);
+  }
+
+
+	public String getInvestmentType(){
+		return this.investmentType;
+	}
+
+
+	public double getBalance(){ return this.startAmount;}
+
+}
