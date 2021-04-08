3673d95c83e7b34554af8b48f49ae5813d2b79dd
Floris van Rijn <florisvanrijn1@gmail.com>
2021-04-08 11:09:29 +0200

    Changes made to investingAccount class include: 1. Change of class name within the file as the classname needs to be the same as the file name 2. Fix of viewPotentialReturns method 3. Fix of viewInvestmentTypes method

--- src/server/investingAccount.java
+++ src/server/investingAccount.java
@@ -5,23 +5,23 @@
 
 package server;
 
-public class Investing {
-	
-	private String investmentType;
-  //initial options: Guaranteed Savings Account Returns, Low-risk investment portfolio, Medium-risk investment portfolio
-	
-  private double startAmount;
-  
-	// come back to savings goal to incorporate it later : private double mySavingsGoal;
-  
-  private double returnRate;
-  //investment rate of return - assuming it's fixed and guaranteed for now
-  //initial options - per year rate of return: 
-  //Guaranteed Savings Account Returns = 1%
-  //Low-risk investment portfolio: %2
-  //Medium-risk investment portfolio: %2.75
+public class investingAccount {
 
-	public Account(String investmentType, double startAmount, double returnRate) {
+	private String investmentType;
+	//initial options: Guaranteed Savings Account Returns, Low-risk investment portfolio, Medium-risk investment portfolio
+
+	private double startAmount;
+
+	// come back to savings goal to incorporate it later : private double mySavingsGoal;
+
+	private double returnRate;
+	//investment rate of return - assuming it's fixed and guaranteed for now
+	//initial options - per year rate of return:
+	//Guaranteed Savings Account Returns = 1%
+	//Low-risk investment portfolio: %2
+	//Medium-risk investment portfolio: %2.75
+
+	public void Account(String investmentType, double startAmount, double returnRate) {
 		this.investmentType = investmentType;
 		this.startAmount = startAmount;
 	}
@@ -35,17 +35,17 @@ public class Investing {
 	public void investMoney(double amount ){
 		this.startAmount += amount;
 	}
-  
-  public String viewInvestmentTypes() {
-    
-    return ("Thanks for choosing to invest and grow your Savings with NewBank. Here are the investment options we currently offer:" + "1. Guaranteed Savings Account Returns = 1% \n"
-  + "2. Low-risk investment portfolio: %2 \n" + "3. Medium-risk investment portfolio: %2.75 \n"
-  }
-  
-  //option for client to view potnetial reutrns from an investment choice over a provided a number of years (add investment type as an arugment later - currently this only answers for investment type 1)
-  public void viewPotentialReturns(double years){
-    this.startAmount * ((1+0.01) * years);
-  }
+
+	public String viewInvestmentTypes() {
+
+		return ("Thanks for choosing to invest and grow your Savings with NewBank. Here are the investment options we currently offer:" + "1. Guaranteed Savings Account Returns = 1% \n"
+				+ "2. Low-risk investment portfolio: %2 \n" + "3. Medium-risk investment portfolio: %2.75 \n");
+	}
+
+	//option for client to view potential returns from an investment choice over a provided a number of years (add investment type as an argument later - currently this only answers for investment type 1)
+	public void viewPotentialReturns(double years){
+		this.startAmount = this.startAmount * ((1+0.01) * years);
+	}
 
 
 	public String getInvestmentType(){
